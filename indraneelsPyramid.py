ed9ae805958f6350733597ec8a4307ffb4a14683
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2021-12-18 15:40:15 +0530

    Create indraneelsPyramid.py

---
+++ indraneelsPyramid.py
@@ -0,0 +1,29 @@
+def pyramid (A):
+
+    A.sort()
+
+    curr = 0
+    counter = 0
+
+    for i in range(len(A)):
+        prod = A[i]
+        minim = min(prod)
+
+        if minim >= curr:
+            counter=counter+1
+
+        if counter == curr+1:
+            counter = 0
+            curr = curr + 1
+    
+    return int((curr*(curr+1))/2)
+
+N = int(input())
+A = []
+
+for _ in range(N):
+    new = list(map(int, input().split()))
+    new.sort()
+    A.append(new)
+
+print(pyramid(A))
