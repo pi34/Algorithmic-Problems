9110742879dad3a39d42196efa0fe467b96f462b
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2021-12-21 20:53:49 +0530

    Create permCycle.py

---
+++ permCycle.py
@@ -0,0 +1,25 @@
+def tiles (N, A):
+
+    arr = []
+    cycles = []
+    queue = []
+    counter = []
+    currt = 0
+
+    def add (n):
+        if n is currt:
+            counter.append(n)
+            cycles.append(counter)
+            counter = []
+            return
+        arr.remove(n)
+        curr = A[n]
+        counter.append(curr)
+        queue.append(curr)
+
+        add(curr-1)
+
+    for i in arr:
+        add()
+
+        
