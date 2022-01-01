b092c34d56ddcae212e9b75ec32194f6d1eb1bcd
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-01-01 18:42:54 +0530

    Create cowCode.cpp

---
+++ cowCode.cpp
@@ -0,0 +1,32 @@
+// Solution to this problem: http://usaco.org/index.php?page=viewproblem2&cpid=692.
+
+#include <iostream>
+#include <string.h>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+
+    int n;
+    cin >> n;
+
+    string str;
+    cin >> str;
+
+    int len = str.size();
+    int calc = (log((n-1)/ len)+1) / log(2);
+    int powr = pow(2, calc) * len;
+    
+    int i = powr;
+    int a = n;
+
+    while (a >= len) {
+        int curr = i - a + 1;
+        i = i / 2;
+        a = i - curr;
+    }
+
+    cout << str[a-1] << endl;
+
+}
