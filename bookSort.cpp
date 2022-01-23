894cb0b274928eb815dae9da44b68509323a80a5
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-01-23 14:34:42 +0530

    Create bookSort.cpp

---
+++ bookSort.cpp
@@ -0,0 +1,27 @@
+// Solution to this: https://www.iarcs.org.in/inoi/contests/sep2005/Advanced-1.php.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main ()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+
+    for (int i = 0; i < n; ++i) {
+        int curr;
+        cin >> curr;
+        int indx = upper_bound(arr.begin(), arr.end(), curr) - arr.begin();
+        if (indx >= arr.size()) {
+            arr.push_back(curr);
+        } else {
+            arr[indx] = curr;
+        };
+    }
+
+    cout << n - arr.size() << endl;
+}
