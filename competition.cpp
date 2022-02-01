290bfefe949575a61fe85d34be5823116d2a6735
Riya Manchanda <59563727+pi34@users.noreply.github.com>
2022-02-01 08:35:07 +0530

    Create competition.cpp

---
+++ competition.cpp
@@ -0,0 +1,23 @@
+// https://oj.uz/problem/view/NOI17_bestplace
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int n;
+    cin >> n;
+
+    int X = 0;
+    int Y = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int x, y;
+        cin >> x >> y;
+        X = X + x;
+        Y = Y + y;
+    }
+
+    cout << (X/n) << " " << (Y/n) << "\n";
+}
